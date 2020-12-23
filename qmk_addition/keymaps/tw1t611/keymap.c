9546209e4c377edcb829638be4c6f9ca8e420a34
shiftux <shiftux@gmail.com>
2020-12-23 10:26:24 +0100

    replace qmk parts

--- qmk_addition/keymaps/tw1t611/keymap.c
+++ qmk_addition/keymaps/tw1t611/keymap.c
@@ -1,77 +1,36 @@
 #include QMK_KEYBOARD_H
+#include "keymap_german.h"
 
-extern keymap_config_t keymap_config;
-
-#define _QWERTZ 0 // standard layer
-#define _SYMB 1   // symbols layer
-
-void keyboard_post_init_user(void) {
-  // Customise these values to desired behaviour
-  debug_enable=true;
-  debug_matrix=true;
-  debug_keyboard=true;
-  //debug_mouse=true;
+enum {
+  QWERTZ = 0,
+  MOD,
 };
-
-enum custom_keycodes {
-  QWERTZ = SAFE_RANGE,
-  SYMB,
-};
-
-// Shortcut to make keymap more readable
-#define KC_BKSL KC_BSLASH
-#define SYM_L   MO(_SYMB)
-
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-  [_QWERTZ] = LAYOUT(
-  //                               ┌────────┐                                                            ┌────────┬────────┐
-                                    KC_MUTE,                                                              KC_MPLY, KC_MNXT,
-  //┌────────┬────────┬────────┬───┼────┬───┼────┬────────┐                                           ┌──┼─────┬──┼─────┬──┼─────┬────────┬────────┬────────┐
-     KC_ESC,   KC_1,    KC_2,    KC_3,   KC_4,    KC_5,                                                 KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+  [QWERTZ] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     _______ ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,                                            KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,_______ ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TAB,   KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,    KC_GRV,                            KC_MINS,  KC_Z,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
+     KC_ESC  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,_______   ,                        _______ ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,DE_AE   ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_CAPS,  KC_A,    KC_S,    KC_D,   KC_F,    KC_G,    KC_EQL,                            KC_RBRC,   KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+     KC_TAB  ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,KC_BSPC ,                          KC_DEL  ,KC_H    ,KC_J    ,KC_K    ,KC_L    ,DE_EQL  ,DE_OE   ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT,  KC_Y,    KC_X,    KC_C,   KC_V,    KC_B,    KC_PGDN,  KC_PGUP,        KC_HOME, KC_END,    KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+     DE_UNDS ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,KC_LCTL ,KC_BSPC ,        KC_DEL  ,KC_RCTL ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,DE_SS   ,DE_UE   ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
-     SYM_L   , KC_LCTL, KC_LALT, KC_LT,        KC_DEL,     KC_LGUI, KC_SPACE,        KC_ENT  , KC_SPC,     KC_RALT,       KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT
+     _______ ,_______ ,_______ ,_______ ,     KC_LALT ,    KC_SPC  ,KC_LSFT ,        MO(MOD) ,KC_ENT  ,    KC_LGUI ,     _______ ,_______ ,_______  ,_______ 
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
   ),
-
-
-  [_SYMB] = LAYOUT(
-   //                              ┌────────┐                                                            ┌────────┬────────┐
-                                    KC_MPRV,                                                              XXXXXXX, XXXXXXX,
-  //┌────────┬────────┬────────┬───┼────┬───┼────┬────────┐                                           ┌──┼─────┬──┼─────┬──┼─────┬────────┬────────┬────────┐
-     RESET    ,KC_F1   ,KC_F2   ,KC_F3   ,KC_F4   ,KC_F5,                                               KC_F6   ,KC_F7   ,KC_F8   ,KC_F9   ,KC_F10  ,KC_F11  ,
+  [MOD] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     KC_F12  ,KC_F1   ,KC_F2   ,KC_F3   ,KC_F4   ,KC_F5   ,                                            KC_F6   ,KC_F7   ,KC_F8   ,KC_F9   ,KC_F10  ,KC_F11,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,KC_F12 ,                           XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX  ,
+     DE_CIRC ,DE_QUOT ,DE_DQOT ,DE_LCBR ,DE_RCBR ,DE_GRV  ,RGB_MOD ,                          RESET   ,DE_PERC ,DE_PLUS ,DE_MINS ,DE_ASTR  ,DE_SLSH ,DE_BSLS ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     XXXXXXX ,XXXXXXX ,XXXXXXX  ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,                          XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
+     DE_TILD ,DE_EXLM ,DE_DLR  ,DE_LPRN ,DE_RPRN ,DE_AMPR ,RGB_TOG ,                          _______ ,KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RGHT ,DE_QST  ,DE_PIPE ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX  ,XXXXXXX ,XXXXXXX ,        XXXXXXX ,XXXXXXX ,KC_TILD ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
+     _______ ,DE_AT   ,DE_EURO ,DE_LBRC ,DE_RBRC ,_______ ,RGB_M_P ,_______ ,        _______ ,_______ ,DE_HASH ,DE_LESS ,DE_SCLN ,DE_COLN ,DE_MORE ,DE_PARA ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
-     XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,     XXXXXXX ,    XXXXXXX ,XXXXXXX ,        XXXXXXX ,XXXXXXX ,    XXXXXXX ,     XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX
+     _______ ,_______ ,_______ ,_______ ,     _______ ,    _______ ,_______ ,        _______ ,_______ ,    _______ ,     _______ ,_______ ,_______ ,_______ 
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
   )
-
 };
-
-void encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) { /* First encoder */
-        if (clockwise) {
-            tap_code(KC_VOLD);
-        } else {
-            tap_code(KC_VOLU);
-        }
-    } else if (index == 1) { /* Second encoder */
-        if (clockwise) {
-            tap_code(KC_WH_D);
-        } else {
-            tap_code(KC_WH_U);
-        }
-    }
-}
